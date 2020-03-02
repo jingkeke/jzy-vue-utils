6a9b8ee28511aa8fc4b7056464ed95eea72caa5e
jingkeke <zhengyi.jing@163.com>
2020-03-02 16:04:13 +0800

    Camerashow.vue

---
+++ src/CameraShow.vue
@@ -0,0 +1,38 @@
+<template>
+  <span ref='countup'></span>
+</template>
+
+<script>
+import { CountUp } from 'countup.js';
+
+export default {
+  name: 'CountTo',
+  props:{
+      endVal:{
+        type:Number,
+        required:true,
+      },
+      options:{
+        type:Object,
+        required:false
+      }
+  },
+  data () {
+    return {
+      numAnim:null
+    }
+  },
+  mounted(){
+    this.initCountUp()
+  },
+  methods:{
+    initCountUp(){
+      this.numAnim = new CountUp(this.$refs.countup,this.endVal ,this.options)
+      this.numAnim.start();
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
